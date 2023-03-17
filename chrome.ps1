7fcc801046bd136025599a3d5714948cf1ed0a8b
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-03-17 11:09:24 +0700

    Update chrome.ps1

--- chrome.ps1
+++ chrome.ps1
@@ -2,9 +2,13 @@
 New-Item -Path $env:temp\automation -ItemType Directory -Force -ErrorAction:SilentlyContinue
 $path = Join-Path $env:temp\automation chrome_installer.exe
 
-#Download the Google Chrome installer.
+#Download and install the Google Chrome.
 $url = 'https://dl.google.com/chrome/install/latest/chrome_installer.exe'
 (new-object System.Net.WebClient).DownloadFile($url,$path)
+Invoke-Expression "$path /silent /install"
 
-#Perform a silent installation of Google Chrome.
+#Download and install Mozilla Firefox.
+$path = Join-Path $env:temp\automation firefox_installer.exe
+$url = 'https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US'
+(new-object System.Net.WebClient).DownloadFile($url,$path)
 Invoke-Expression "$path /silent /install"
