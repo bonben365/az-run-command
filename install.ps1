3c29aaf3ad2e2332577cd4cd21f5bc59d52fd8f1
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-03-17 09:56:06 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,10 +1,10 @@
 #Create a temporary directory to store Google Chrome.
-md -Path $env:temp\chromeinstall -erroraction SilentlyContinue | Out-Null
-$Download = join-path $env:temp\chromeinstall chrome_installer.exe
+New-Item -Path $env:temp\automation -ItemType Directory -Force -ErrorAction:SilentlyContinue
+$path = Join-Path $env:temp\automation chrome_installer.exe
 
 #Download the Google Chrome installer.
 $url = 'https://dl.google.com/chrome/install/latest/chrome_installer.exe'
-Invoke-WebRequest $url  -OutFile $Download
+(new-object System.Net.WebClient).DownloadFile($url,$path)
 
 #Perform a silent installation of Google Chrome.
-Invoke-Expression "$Download /silent /install"
+Invoke-Expression "$path /silent /install"
